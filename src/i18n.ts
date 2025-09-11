ee215b511711f8e99291dce9869f18c605d1ef5b
Lucas Braeschke <lucas.braeschke@zeiss.com>
2025-09-11 15:21:24 +0200

    Add search icon

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,12 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import icon from "./search-icon.svg";
 
 i18n.use(initReactI18next).init({
   resources: {
     en: {
       translation: {
-        searchResultSummary: 'Your search is: <strong>"{{search}}"</strong>',
+        searchResultSummary: 'Your search <img src="' + icon + '" alt="search icon" /> is: "{{search}}"',
         searchPlaceholder: 'Search for skills, names, departments...',
         searchButton: 'Search',
       },
